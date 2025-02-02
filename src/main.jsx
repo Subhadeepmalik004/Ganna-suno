bbbfe897fc9d21232beaf8f6713d1d8dde3426a1
Subhadeep Malik <146656212+Subhadeepmalik004@users.noreply.github.com>
2025-02-02 23:28:23 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from "react-router-dom"
 import './index.css'
 import './App.css'
 import App from './App.jsx'
 // import { Menu } from './Menu.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-  <App/>
-  </StrictMode>,
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
 )
